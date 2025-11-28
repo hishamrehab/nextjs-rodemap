ba255350957e9ea359ebfd467e2dd8e7f0fa1887
hishamrehab <hishamrehab30@gmail.com>
2025-11-28 05:05:44 +0200

    Add Styling & Using DUMMY DATA

--- 2 - Routing & Page Rendering/app/layout.js
+++ 2 - Routing & Page Rendering/app/layout.js
@@ -10,8 +10,11 @@ export default function RootLayout({ children }) {
  return (
     <html lang="en">
       <body>
-         <MainHeader />
+        <div id='page'>
+      <MainHeader />
         {children}
+        </div>
+   
         </body>
     </html>
   )
--- 2 - Routing & Page Rendering/app/news/page.js
+++ 2 - Routing & Page Rendering/app/news/page.js
@@ -1,23 +1,24 @@
 import Link from "next/link";
+import { DUMMY_NEWS } from "@/dummy-news";
+
 
 export default function  NewsPage() {
     return <>
     <h1>
         News Page
     </h1>
-    <ul>
-        <li>
-            <Link to="/news/first-news">Fist News Item</Link>
+    <ul className="news-list">
+      {DUMMY_NEWS.map((newsItem) => (
+        <li key={newsItem.id}>
+            <Link href={`/news/${newsItem.slug}`}>
+              <img src={`/images/news/${newsItem.image}`} alt={newsItem.titk} />
+           <span>{newsItem.title}</span>
+            </Link>
         </li>
 
-         <li>
-              <Link to="/news/second-news">Second News Item</Link>
-        </li>
-
-
-         <li>
-            <Link to="/news/third-news">Third News Item</Link>
-        </li>
+      ))}
+      
+        
 
     </ul>
     </>
