2ee2d0705fb317b1a90ffc91f419ee51b2835d59
hishamrehab <hishamrehab30@gmail.com>
2025-12-18 19:08:58 +0200

    Upload & Storing Images

--- 4- Mutating Data/mutating-data/actions/posts.js
+++ 4- Mutating Data/mutating-data/actions/posts.js
@@ -1,6 +1,6 @@
 "use server";
-import { storePost } from "@/lib/posts";
-
+import { uploadImage } from "@/lib/cloudinary";
+    import { storePost } from "@/lib/posts";
     import { redirect } from "next/navigation";
 
 
@@ -28,8 +28,17 @@ import { storePost } from "@/lib/posts";
     throw { errors };
   }
 
+  let imageUrl = '';
+
+    try {
+     imageUrl = await uploadImage(image);
+    }catch (error) {
+    throw new Error('Image upload failed. post was not created. Please try again later.');
+    }
+  
+
   await storePost({
-    imageUrl:"" ,
+    imageUrl:imageUrl,
     title,
     content,
     userId: 1,
