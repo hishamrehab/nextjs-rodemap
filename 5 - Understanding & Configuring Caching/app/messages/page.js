bc9ced15b137541f016eea8b85fca9d13bf34037
hishamrehab <hishamrehab30@gmail.com>
2026-02-13 22:24:09 +0200

    Contorlling Data Chache

--- 5 - Understanding & Configuring Caching/app/messages/page.js
+++ 5 - Understanding & Configuring Caching/app/messages/page.js
@@ -1,6 +1,13 @@
 import Messages from '@/components/messages';
+import { unstable_noStore } from "next/chace"
+
+// export const revalidate = 5;
+// export const dynamic = "force-dynamic";
+
+
 
 export default async function MessagesPage() {
+  // unstable_noStore();
   const response = await fetch('http://localhost:8080/messages', {
     next: {
       revalidate: 5
