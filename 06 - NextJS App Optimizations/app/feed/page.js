79d7972d2422c7eb80f7b3d0fc82166d9bad1371
hishamrehab <hishamrehab30@gmail.com>
2026-02-15 10:34:04 +0200

    configuring Stataic Page Metadata

--- 06 - NextJS App Optimizations/app/feed/page.js
+++ 06 - NextJS App Optimizations/app/feed/page.js
@@ -1,6 +1,14 @@
 import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
+
+export const metadata = {
+  title: "All Posts",
+  description: ""
+}
+
+
+
 export default async function FeedPage() {
   const posts = await getPosts();
   return (
