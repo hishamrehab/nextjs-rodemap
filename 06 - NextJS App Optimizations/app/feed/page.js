c23de1b14c65aa5c588b11d8ce16e0a383ba9055
hishamrehab <hishamrehab30@gmail.com>
2026-02-15 10:44:56 +0200

    configuring Dynamic Page Metadata

--- 06 - NextJS App Optimizations/app/feed/page.js
+++ 06 - NextJS App Optimizations/app/feed/page.js
@@ -2,11 +2,20 @@ import Posts from '@/components/posts';
 import { getPosts } from '@/lib/posts';
 
 
-export const metadata = {
-  title: "All Posts",
-  description: ""
-}
+// export const metadata = {
+//   title: "All all our X posts",
+//   description: "Browse all our posts."
+// }
 
+
+export async function generateMetadata() {
+  const posts = await getPosts();
+  const numberOfPosts = posts.length;
+  return {
+    title: `Browse all our ${numberOfPosts} posts.`,
+    description: "Browse all our posts"
+  }
+}
 
 
 export default async function FeedPage() {
